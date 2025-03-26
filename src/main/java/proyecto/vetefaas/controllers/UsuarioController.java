72f36a44bd506a82030f3a1d7789d59812b86328
pabloperezarcos <pabloperezarcos@gmail.com>
2025-03-25 23:32:07 -0300

    Update UsuarioController.java

--- src/main/java/proyecto/vetefaas/controllers/UsuarioController.java
+++ src/main/java/proyecto/vetefaas/controllers/UsuarioController.java
@@ -1,5 +1,28 @@
 package proyecto.vetefaas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import proyecto.vetefaas.services.UsuarioService;
+import proyecto.vetefaas.dtos.UsuarioDto;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<String> crearUsuario(@RequestBody UsuarioDto usuarioDto) {
+        String resultado = usuarioService.crearUsuario(usuarioDto);
+        return ResponseEntity.ok(resultado);
+    }
+
+    @PostMapping("/asignar-rol")
+    public ResponseEntity<String> asignarRol(@RequestParam String userId, @RequestParam String rolId) {
+        String resultado = usuarioService.asignarRol(userId, rolId);
+        return ResponseEntity.ok(resultado);
+    }
 }
